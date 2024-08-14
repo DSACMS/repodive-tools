e8708ff87a960b5b5d7983f0cfda2da76077569c
Isaac Milarsky <imilarsky@gmail.com>
2024-08-14 10:47:54 -0500

    add colorization scripts
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

---
+++ colorize_highlight_names.py
@@ -0,0 +1,28 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Metrics script to update data')
+parser.add_argument('log_file')
+args = parser.parse_args()
+
+log_file = args.log_file
+
+colors = ["#474747","#E4E4E4"]
+
+
+names = ["Brandon Yee", "Ricardo Zamora","CreativeNick"]
+
+with open(log_file, 'r') as f:
+    log_lines = f.readlines()
+
+current_color = 0
+for line in log_lines:
+    
+    for name in names:
+        if name in line:
+            color = colors[1]
+        else:
+            color = colors[0]
+
+    new_line = line.strip() + f"|{color}"
+    print(new_line)
